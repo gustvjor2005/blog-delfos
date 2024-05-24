1b4b0903cd2d8aaaf22ecf3c4c584a331e029edd
gustavo.choque <gustvjor2005@gmail.com>
2024-05-24 18:02:36 -0500

    using async and await in javascript

--- js/c.js
+++ js/c.js
@@ -3,84 +3,67 @@ var objInd = document.getElementById("uri").value;
 
 loadComment();
 
-fobj.addEventListener('submit', (event) => {
-
+fobj.addEventListener("submit", (event) => {
   event.preventDefault();
   var objUser = document.getElementById("namef").value;
   var objEmail = document.getElementById("emailf").value;
   var objComment = document.getElementById("comment").value;
 
   const dataJson = {
-    "username": objUser,
-    "section": objInd,
-    "details": objComment,
-    "email": objEmail
-  };
+    username: objUser,
+    section: objInd,
+    details: objComment,
+    email: objEmail,
+  };  
 
-  const fpromiseP = fetch("/comment", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(dataJson)
-  });
-
-  fpromiseP.then((response) => {
-
-    if (!response.ok) {
-      throw new Error("Ocurrio un error");
-    } else {
-      return response.text();
-    }
-  })
-    .then((data) => {
-      document.getElementById("namef").value = "";
-      document.getElementById("emailf").value = "";
-      document.getElementById("comment").value = "";
-      loadComment();
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error('Error:', error)
-    });
-
+  saveComment(dataJson);
 });
 
-function loadComment() {
-
-  console.log("load comment");
-
-  const fpromiseG = fetch("/comment", {
-    headers: {
-      'ind': objInd
-    }
-  });
-
-  fpromiseG.then((response) => {
-
-    if (!response.ok) {
-      throw new Error("Error de invocacion al api");
-    } else {
-      return response.json();
-    }
-
-  })
-    .then((data) => {
-      var obj = document.getElementById("commentlist");
-      obj.innerHTML = buildComment(data);
-
-      console.log(data[0]);
-
+async function saveComment(dataJson) {
+  try {
+    console.log("saving comment");
+    await fetch("/comment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataJson),
     });
 
+    document.getElementById("namef").value = "";
+    document.getElementById("emailf").value = "";
+    document.getElementById("comment").value = "";
+    loadComment();
+  } catch (error) {
+    console.error("Could no save comments:", error);
+  }
+}
+
+async function loadComment() {
+  try {
+    console.log("loading comment");
+
+    const fpromiseG = await fetch("/comment", {
+      headers: {
+        ind: objInd,
+      },
+    });
+
+    const data = await fpromiseG.json();
+    commentListObject = document.getElementById("commentlist");
+    commentListObject.innerHTML = buildComment(data);
+  } catch (error) {
+    console.error("Could no get comments:", error);
+  }
 }
 
 function buildComment(data) {
-
   var c = "";
 
   for (var i = 0; i < data.length; i++) {
-    c = c + `<li id="comment01">
+    c =
+      c +
+      `<li id="comment01">
       <cite>${data[i].username} (${data[i].day.substring(0, 10)})</cite>
       <p>${data[i].details}</p>
       </li>`;

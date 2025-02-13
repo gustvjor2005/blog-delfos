9836ffdcc49d3cedd5f6b3f2bc9f17e193412d81
gustavo.choque <gustvjor2005@gmail.com>
2025-02-12 22:21:39 -0500

    using https

--- transfer.sh
+++ transfer.sh
@@ -6,7 +6,7 @@
 KEY="suntzukp.pem"
 USER="ubuntu"
 HOST="54.94.176.119"
-REMOTE_DIR="/var/www/odin.com/html/"
+REMOTE_DIR="/var/www/gchoque.de/html/"
 
 # Run scp commands
 scp -i "$KEY" gcd.html $USER@$HOST:$REMOTE_DIR
@@ -14,3 +14,14 @@ scp -i "$KEY" -r blog $USER@$HOST:$REMOTE_DIR
 scp -i "$KEY" -r fa $USER@$HOST:$REMOTE_DIR
 
 echo "All files have been uploaded!"
+
+# Restart Nginx on the remote server
+echo "Restarting Nginx..."
+ssh -i "$KEY" $USER@$HOST << EOF
+  sudo systemctl restart nginx
+  if [ \$? -eq 0 ]; then
+    echo "Nginx restarted successfully!"
+  else
+    echo "Failed to restart Nginx!" >&2
+  fi
+EOF
